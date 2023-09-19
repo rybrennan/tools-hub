5eddf8ce29d7f08c4d006fbee7d8e3d9843a8efa
Ryan Brennan <ryanbrennanbc1@gmail.com>
2023-09-19 16:53:43 -0700

    create todo  Please enter the commit message for your changes. Lines starting

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Any requests that don't match a static file will be sent to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
